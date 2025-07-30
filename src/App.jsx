761c49e4eb2b447e6ab743eec00c8f8c764b248c
nmasters <nmasters52@gmail.com>
2025-07-30 15:03:34 -0400

    initial timer is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState, useRef } from 'react'
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+  const intervalId = useRef(null);
+  const startTimer = () => {
+    intervalId.current = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000)
+  }
+
   return (
-    <div className='bg-red-200'>App</div>
+    <div>
+      <h2>timer app</h2>
+      <p>{count}</p>
+      <button 
+        onClick={() => startTimer()}
+        className="cursor-pointer"
+      >
+        Start
+      </button>
+    </div>
   )
 }
 
